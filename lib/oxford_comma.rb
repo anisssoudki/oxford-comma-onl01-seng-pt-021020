db86da48b03a1273bb01a22c0c3b0417e2bbe6f3
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-13 18:34:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
 
+array = ["fiddleheads","okra","kohlrabi"]
+
+def oxford_comma(array)
+
+ array.pop
+
+array.pop
+array.push(", okra, kohlrabi")
+
+result = array.join
+puts result
+
+
+end
+
+oxford_comma(array)
 end

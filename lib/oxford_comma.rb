140c9b0c5aacf80974d34c116fbe069d71b25201
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-13 19:24:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -23,12 +23,15 @@
 array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
-
-result = array.join(" and ")
-
-
-p result
-
-return result
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
+
 oxford_comma(array)

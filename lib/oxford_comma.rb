f6699166eaf8b55ddae60432724b12220a3b6c3b
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-13 18:53:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,21 @@
 
-array = ["fiddleheads","okra","kohlrabi"]
+# array = ["fiddleheads","okra","kohlrabi"]
 
-def oxford_comma(array)
-array << "and"
-puts array
-array[2],  array[3] = array[3], array[2]
-p array
-result = array.join(", ")
-arr2 = result.split("")
-puts result
-p arr2
-puts arr2.length
-arr2.delete_at(22)
-p arr2
-puts arr2.length
-p arr2.join
-return arr2
-end
+# def oxford_comma(array)
+# array << "and"
+# puts array
+# array[2],  array[3] = array[3], array[2]
+# p array
+# result = array.join(", ")
+# arr2 = result.split("")
+# puts result
+# p arr2
+# puts arr2.length
+# arr2.delete_at(22)
+# p arr2
+# puts arr2.length
+# p arr2.join
+# return arr2
+# end
 
-oxford_comma(array)
+# oxford_comma(array)
